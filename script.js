2e6be34a4579d495067e5280763d3fc603338ca2
ardacoskuunn <ardacskn25@gmail.com>
2026-09-05 15:42:30 +0300

    g1

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const form = document.getElementById("iletisimFormu");
 const durumMesaji = document.getElementById("formDurum");
 
-// Eğer sayfada form varsa (hata vermemesi için) bu işlemi yap
+// Eğer sayfada form varsa bu işlemi yap
 if (form) {
     form.addEventListener("submit", function(event) {
         event.preventDefault(); // Sayfanın yönlenmesini engeller
@@ -44,52 +44,4 @@ if (menuBtn && nav) {
         // Butona basıldığında 'aktif' class'ını ekler veya çıkarır
         nav.classList.toggle("aktif"); 
     });
-// =========================================
-// 1. İLETİŞİM FORMU (Sayfa Yenilenmeden Gönderim)
-// =========================================
-const form = document.getElementById("iletisimFormu");
-const durumMesaji = document.getElementById("formDurum");
-
-// Eğer sayfada form varsa (hata vermemesi için) bu işlemi yap
-if (form) {
-    form.addEventListener("submit", function(event) {
-        event.preventDefault(); // Sayfanın yönlenmesini engeller
-
-        const formData = new FormData(form);
-
-        fetch(form.action, {
-            method: "POST",
-            body: formData,
-            headers: { 'Accept': 'application/json' }
-        })
-        .then(response => {
-            if (response.ok) {
-                durumMesaji.style.display = "block"; // Başarı mesajını göster
-                form.reset(); // Formu temizle
-                
-                // 5 saniye sonra mesajı gizle
-                setTimeout(() => {
-                    durumMesaji.style.display = "none"; 
-                }, 5000);
-            }
-        })
-        .catch(error => {
-            console.log("Mesaj gönderilirken hata oluştu:", error);
-        });
-    });
-}
-
-// =========================================
-// 2. MOBİL MENÜYÜ AÇMA / KAPATMA İŞLEMİ
-// =========================================
-const menuBtn = document.querySelector(".menuBtn");
-const nav = document.querySelector(".nav");
-
-if (menuBtn && nav) {
-    menuBtn.addEventListener("click", () => {
-        // Butona basıldığında 'aktif' class'ını ekler veya çıkarır
-        nav.classList.toggle("aktif"); 
-    });
-
-}
 }
